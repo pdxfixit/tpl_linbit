b6a6320bc1557895b1e45804e5e78a82ea1a04b2
Ben Sandberg <ben@pdxfixit.com>
2012-08-06 18:48:12 +0000

    changes roundup

--- template-overview.php
+++ template-overview.php
@@ -1,13 +1,17 @@
-<img src="<?php echo $gantry->templateUrl . '/template_thumbnail.png';?>" width="206" height="140" class="template-preview" alt=""/>
-<h1><?php echo $gantry->get('template_full_name');?> <span>v<?php echo $gantry->_template->getVersion();?></span></h1>
-<p>Gantry framework default theme is just a small example on how easy you can create your Gantry based themes.</p>
+<img src="<?php echo $gantry->templateUrl . '/template_thumbnail.png'; ?>" width="206" height="140" class="template-preview" alt=""/>
 
-<h2><?php echo $gantry->get('template_full_name');?> Presets</h2>
+<h1><?php echo $gantry->get('template_full_name'); ?> <span>v<?php echo $gantry->_template->getVersion(); ?></span></h1>
+
+<p>This template was originally created in September 2011, for LINBIT USA LLC by Jacob Forrester using the Gantry framework.</p>
+<p>Additional development and packaging provided by <a href="http://www.pdxfixit.com/" target="_blank">PDXfixIT, LLC</a>.</p>
+
+<h2><?php echo $gantry->get('template_full_name'); ?> Presets</h2>
 
 <p>Presets are a pre-defined set of values that can be applied across any of the configuration settings for this theme.
     The Presets Panel can be opened by clicking on 'Presets' on the top right of the screen, clicking a preset option is
     akin to clicking a preset button on your car radio, it effectively sets the preset values in all the fields and you
     can tweak and apply those settings. You can also 'Save Custom Preset as New' to create your own preset.</p>
+
 <h2>Overview of the Gantry Framework</h2>
 
 <p>Gantry is a sophisticated framework with the sole intention of being the best platform to build a solid theme with.
